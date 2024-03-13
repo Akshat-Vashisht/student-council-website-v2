6a9a52086ec5a9ca8d0f4caee435afc28b87777c
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-03-13 23:51:36 +0530

    ADD: event route

---
+++ app/events/page.js
@@ -0,0 +1,12 @@
+import Carousel from "@components/Events/Carousel";
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <Carousel />
+    </div>
+  );
+}
+
+export default page;
