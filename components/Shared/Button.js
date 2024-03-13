e597dc2a611165fd55ca6bc07c5aac874251a922
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-03-13 15:39:13 +0530

    ADD: Button- shared

---
+++ components/Shared/Button.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Button() {
+  return (
+    <div>
+      <button className="justify-center items-center px-16 py-1.5 mt-8 max-w-full text-white whitespace-nowrap bg-stone-950 rounded-[36px] tracking-[2px] w-[359px] max-md:px-5">
+        LEARN MORE
+      </button>
+    </div>
+  );
+}
+
+export default Button;
