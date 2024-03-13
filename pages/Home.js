7579d2cbd1ed7d1657d9ae619b1ca91da4ec35f9
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-03-13 15:39:54 +0530

    ADD: components to Home Page

--- pages/Home.js
+++ pages/Home.js
@@ -1,11 +1,15 @@
 import Hero from "@components/Home/Hero";
+import Impact from "@components/Home/Impact";
 import Scroll from "@components/Home/Scroll.js";
+import What from "@components/Home/What";
 
 function Home() {
   return (
     <div>
       <Hero />
       <Scroll />
+      <What />
+      <Impact />
     </div>
   );
 }
