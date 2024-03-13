0fc3f16382971c08435334c3a10c96ef8ee440a1
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-03-13 16:19:12 +0530

    FIX: Footer Icons and Responsiveness

--- components/Global/Footer.js
+++ components/Global/Footer.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { TiSocialFacebook } from "react-icons/ti";
+import { FaLinkedinIn } from "react-icons/fa6";
+import { IoLogoTwitter } from "react-icons/io5";
 
 function Footer() {
   return (
     <div>
-      {" "}
       <div className="flex flex-col justify-center px-8 py-8 font-bold bg-amber-100 max-md:px-5">
         <div className="flex flex-col justify-center bg-lime-400 rounded-[59.945px] max-md:max-w-full">
           <div className="flex justify-center items-center px-16 py-20 bg-lime-400 rounded-[59.945px] max-md:px-5 max-md:max-w-full">
@@ -27,7 +29,7 @@ function Footer() {
                 </button>
               </div>
               <div className="shrink-0 mt-20 max-w-full h-px bg-white w-[1060px] max-md:mt-10" />
-              <div className="flex gap-5 justify-between items-start self-stretch mt-12 max-md:flex-wrap max-md:mt-10 max-md:max-w-full">
+              <div className="flex gap-5 justify-between items-center self-stretch mt-12 max-md:flex-wrap max-md:mt-10 max-md:max-w-full">
                 <div className="flex flex-col mt-2 text-white whitespace-nowrap">
                   <div className="justify-center items-start self-end px-0.5 py-1 max-w-full text-xl ">
                     <span className="text-white bg-[#004AAD]">Office of</span>
@@ -37,14 +39,24 @@ function Footer() {
                     MIT-ADT University, Pune, India{" "}
                   </div>
                 </div>
-                <div className="flex-auto self-stretch my-auto text-3xl leading-3 text-center text-teal-950">
+                <div className="flex items-center self-stretch my-auto text-xl md:text-2xl sm:text-2xl  leading-3 text-center text-teal-950 flex-wrap">
                   Designed by IDEATE | Developed by GDSC
                 </div>
-                <img
-                  loading="lazy"
-                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/d1a8d7210e3d648a49962e45be318a63640ebf4a9fbaab461331bd9d820d38da?apiKey=6e0b39cac1cf445b93dc7744259e3ead&"
-                  className="shrink-0 max-w-full aspect-[3.85] w-[166px]"
-                />
+
+                <div className="flex gap-3 items-center">
+                  <TiSocialFacebook
+                    size={30}
+                    className="rounded-full border border-white p-2"
+                  />
+                  <FaLinkedinIn
+                    size={30}
+                    className="rounded-full border border-white p-2"
+                  />
+                  <IoLogoTwitter
+                    size={30}
+                    className="rounded-full border border-white p-2"
+                  />
+                </div>
               </div>
             </div>
           </div>
