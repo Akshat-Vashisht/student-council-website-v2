be29b8a9ff9260ab9b0272d6a220f91aa11846b6
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-03-13 19:09:01 +0530

    ADD: Vission component

---
+++ components/Home/Vision.js
@@ -0,0 +1,41 @@
+function Vission() {
+  return (
+    <div>
+      <div className="flex flex-col justify-center p-10 bg-amber-100">
+        <div className="flex flex-col items-center px-16 pt-3.5 pb-20 w-full bg-sky-300 rounded-[33px] max-md:px-5 max-md:max-w-full">
+          <div className="flex flex-col mb-1.5 w-full max-w-[1143px] max-md:max-w-full">
+            <div className="max-md:max-w-full">
+              <div className="flex gap-5 max-md:flex-col max-md:gap-0">
+                <div className="flex flex-col w-[71%] max-md:ml-0 max-md:w-full">
+                  <div className="flex flex-col grow mt-12 text-2xl font-bold text-neutral-700 max-md:mt-10 max-md:max-w-full">
+                    <div className="text-5xl font-black text-justify uppercase leading-[92.71px] text-teal-950 tracking-[4.75px] max-md:max-w-full max-md:text-4xl">
+                      VISION
+                    </div>
+                    <div className="mt-11 max-md:mt-10 max-md:max-w-full">
+                      Lorem ipsum dolor sit amet consectetur. Enim dolor
+                      accumsan aliquet magna ullamcorper lorem accumsan ipsum.
+                      Consectetur tempus pellentesque nulla egestas elementum
+                      mattis ut. Tellus quis faucibus duis pellentesque metus
+                      scelerisque nisi rhoncus fames.{" "}
+                    </div>
+                    <div className="mt-7 max-md:max-w-full">
+                      Lorem ipsum dolor sit amet consectetur. Enim dolor
+                      accumsan aliquet magna ullamcorper lorem accumsan ipsum.
+                      Consectetur tempus pellentesque nulla egestas elementum
+                      mattis ut.{" "}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <button className="justify-center self-start px-14 py-6 mt-12 text-xl font-semibold tracking-wide leading-6 text-black whitespace-nowrap bg-white rounded-[39.459px] max-md:px-5 max-md:mt-10">
+              LEARN MORE
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Vission;
