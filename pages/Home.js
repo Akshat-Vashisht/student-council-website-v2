99d0911bc00cc06eb92ded3bb3d6fac38732767f
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-03-13 15:25:47 +0530

    ADD: Home page

---
+++ pages/Home.js
@@ -0,0 +1,13 @@
+import Hero from "@components/Home/Hero";
+import Scroll from "@components/Home/Scroll.js";
+
+function Home() {
+  return (
+    <div>
+      <Hero />
+      <Scroll />
+    </div>
+  );
+}
+
+export default Home;
