752607787f8643b50121035ba3f5ceaa1cd2229a
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-03-17 01:26:20 +0530

    updates

--- app/team/page.js
+++ app/team/page.js
@@ -3,7 +3,7 @@
 
 import Card from "@components/Team/Card";
 import Star from "@public/assets/Star";
-import TeamJson from "../../public/assets/Team/Team.json"
+import TeamJson from "../../public/assets/Team/Team.json";
 // const presidentData = {
 //   name: "Sankalp",
 //   position: "president",
@@ -28,7 +28,11 @@ const page = async () => {
   return (
     <div className="p-14 space-y-[8rem] bg-[#FFEDBF] overflow-x-hidden">
       <div className="relative w-fit mx-auto my-4">
-        <Star size={170} color={"#F6A342"} position={"-top-[4rem] -left-[4rem]"}/>
+        <Star
+          size={170}
+          color={"#F6A342"}
+          position={"-top-[4rem] -left-[4rem]"}
+        />
         <h1 className="text-5xl font-bold uppercase text-center text-[#083133] relative">
           Say <span className="text-[#FF61C0]"> hello </span> to <br /> council
           Member
@@ -37,7 +41,11 @@ const page = async () => {
 
       {/* <Test data={data}/> */}
       <div className="flex flex-col justify-center items-center gap-y-[5rem] relative">
-      <Star size={240} color={"#79E0E6"} position={"-bottom-[4rem] -right-[10rem]"}/>
+        <Star
+          size={240}
+          color={"#79E0E6"}
+          position={"-bottom-[4rem] -right-[10rem]"}
+        />
         <h1 className="text-black text-4xl font-black text-center">
           Faculty Coordinator
         </h1>
@@ -47,26 +55,26 @@ const page = async () => {
       <div className="flex flex-col justify-center items-center gap-y-[5rem] ">
         <h1 className="text-black text-4xl font-black text-center relative uppercase">
           <p className="z-10 relative"> Core Team</p>
-        <Star size={70} color={"#FF61C0"} position={"-top-4 -right-3 z-0"}/>
-
+          <Star size={70} color={"#FF61C0"} position={"-top-4 -right-3 z-0"} />
         </h1>
 
         <div className="flex flex-wrap gap-10 justify-evenly ">
-          {TeamJson.filter(member=>member.role==="coreTeam").map((data) => {
-            return <Card data={data} />;
-          })}
+          {TeamJson.filter((member) => member.role === "coreTeam").map(
+            (data) => {
+              return <Card data={data} />;
+            }
+          )}
         </div>
       </div>
- 
+
       <div className="flex flex-col justify-center items-center gap-y-[5rem]">
         <h1 className="text-black text-4xl font-black text-center relative uppercase">
-        <p className="z-10 relative"> Team</p>
-        <Star size={70} color={"#79E0E6"} position={"-top-4 -left-3 z-0"}/>
-
+          <p className="z-10 relative"> Team</p>
+          <Star size={70} color={"#79E0E6"} position={"-top-4 -left-3 z-0"} />
         </h1>
 
         <div className="flex flex-wrap gap-10 justify-evenly">
-          {TeamJson.filter(member=>member.role==="team").map((data) => {
+          {TeamJson.filter((member) => member.role === "team").map((data) => {
             return <Card data={data} />;
           })}
         </div>
