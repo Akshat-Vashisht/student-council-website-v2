6fa6eda147d8a367cad88a97d6e58043620651ef
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-03-16 23:41:32 +0530

    Add: changes

--- components/Events/Upcoming.js
+++ components/Events/Upcoming.js
@@ -1,27 +1,51 @@
-"use client";
+import { memo } from "react";
+
+const RectangleIcon = () => (
+  <img
+    src="https://cdn.builder.io/api/v1/image/assets/TEMP/72e467b269ee78d4b2885ed6b7646e0cf54b101b8c374ba67238c06140a9691b?apiKey=6e0b39cac1cf445b93dc7744259e3ead&"
+    className="w-full aspect-[1.03] fill-yellow-300 max-w-[186px]"
+  />
+);
 
 function Upcoming() {
-  const colors = ["bg-red-500", "bg-blue-500", "bg-green-500"];
+  // Mock events data
+  const events = [
+    {
+      id: 1,
+      date: "29",
+      month: "Feb",
+      title: "Event 1",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac eleifend metus. Cras sed purus eu ligula tincidunt placerat. In luctus augue nec varius maximus. Pellentesque egestas augue est, eget.",
+    },
+    {
+      id: 2,
+      date: "15",
+      month: "Mar",
+      title: "Event 2",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac eleifend metus. Cras sed purus eu ligula tincidunt placerat. In luctus augue nec varius maximus. Pellentesque egestas augue est, eget.",
+    },
+    {
+      id: 3,
+      date: "20",
+      month: "Apr",
+      title: "Event 3",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac eleifend metus. Cras sed purus eu ligula tincidunt placerat. In luctus augue nec varius maximus. Pellentesque egestas augue est, eget.",
+    },
+  ];
 
   return (
-    <div>
-      <div className="bg-amber-100">
-        <div className="text-center text-black text-6xl font-black font-sans uppercase leading-10 tracking-widest p-20">
-          Upcoming Events
-        </div>
-        <div className="p-10">
-          <div className="flex flex-col items-center">
-            {colors.map((color, index) => (
-              <div
-                key={index}
-                className={`w-full h-48 my-4 rounded-lg shadow-lg ${color}`}
-              ></div>
-            ))}
-            </div>
-          </div>
-        </div>
-      </div>
+    <div
+      className={`use client bg-[#FFECC1] w-full mx-auto flex flex-col items-start relative`}
+    >
+      <div>Upcoming Events</div>
+      {events.map((event) => (
+        <div key={event.id} className="relative w-full mx-auto my-8"></div>
+      ))}
+    </div>
   );
 }
 
-export default Upcoming;
+export default memo(Upcoming);
