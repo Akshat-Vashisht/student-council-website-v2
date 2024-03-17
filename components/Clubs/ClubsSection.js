48b198a234929e293a3468764db3fad2ab3d5d53
Shankyy <sankalpt92@gmail.com>
2024-03-17 17:32:43 +0530

    feat(component): create club section component

---
+++ components/Clubs/ClubsSection.js
@@ -0,0 +1,32 @@
+"use client";
+import Card from "@components/Shared/Card";
+import Clubs from "../../public/assets/Clubs/Clubs.json";
+import { useState } from "react";
+
+
+const ClubsSection = ({ tags }) => {
+    const [activeTab, setActiveTab] = useState("")
+  return (
+    <div className="">
+      <div className="flex flex-wrap gap-x-3 py-14 mx-[3rem]">
+        {tags.map((tag) => (
+          <span
+            key={tag}
+            onClick={()=>setActiveTab(tag)}
+            className={` cursor-pointer border transition-all duration-150 ${activeTab===tag ? " text-white bg-[#FF61C0]":" text-[#FF61C0]"} border-[#FF61C0] font-medium p-1 px-2 rounded-full`}
+          >
+            {tag}
+          </span>
+        ))}
+      </div>
+            
+      <div className="flex flex-wrap gap-10 justify-center items-center p-10">
+        {Clubs.map(club=>(
+            <Card data={club} useAs="clubs"/>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ClubsSection;
