207f2e5f2341e94a2fb3c477a4d366af78d6c5e3
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-03-19 02:44:48 +0530

    fix: responsiveness

--- components/Home/What.js
+++ components/Home/What.js
@@ -16,9 +16,9 @@ function What() {
             <div className="mt-8">
               <p className="max-w-full">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industrys standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book. It has
+                industry. Lorem Ipsum has been the industrys standard dummy text
+                ever since the 1500s, when an unknown printer took a galley of
+                type and scrambled it to make a type specimen book. It has
                 survived not only five centuries, but also the leap into
                 electronic typesetting, remaining essentially unchanged.
               </p>
@@ -37,9 +37,9 @@ function What() {
             <div className="mt-8">
               <p className="max-w-full">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industrys standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book. It has
+                industry. Lorem Ipsum has been the industrys standard dummy text
+                ever since the 1500s, when an unknown printer took a galley of
+                type and scrambled it to make a type specimen book. It has
                 survived not only five centuries, but also the leap into
                 electronic typesetting, remaining essentially unchanged.
               </p>
