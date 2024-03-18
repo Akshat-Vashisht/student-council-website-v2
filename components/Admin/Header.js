73f5739b65ab13a4af4aa8bc6e8e9e02e77d6954
Shankyy <sankalpt92@gmail.com>
2024-03-18 13:01:52 +0530

    feat: custom sign-out button

--- components/Admin/Header.js
+++ components/Admin/Header.js
@@ -1,34 +1,36 @@
-'use client'
+"use client";
 import { CiSquarePlus, CiTrash, CiEdit } from "react-icons/ci";
+import { MdLogout } from "react-icons/md";
+import { SignOutButton } from "@clerk/nextjs";
 
-
-export default function Header(){
-    const options = [
-        {
-            name: "New Field",
-            icon: <CiSquarePlus size={19}/>
-        },
-        {
-            name: "Delete Field",
-            icon: <CiTrash size={19}/>
-            
-        },
-        {
-            name: "Edit Field",
-            icon: <CiEdit size={19}/>
-            
-        }
-    ]
-    return(
-        <div className="p-3">
-            <div className="flex gap-6 justify-end">
-                {options.map(option=>(
-                     <span className="flex items-center gap-1 text-sm hover:bg-[#25231d] p-1 px-3 rounded-md cursor-pointer">
-                        {option.icon}
-                        {option.name}
-                     </span>
-                ))}
-            </div>
-        </div>
-    )
+export default function Header() {
+  const options = [
+    {
+      name: "New Field",
+      icon: <CiSquarePlus size={19} />,
+    },
+    {
+      name: "Delete Field",
+      icon: <CiTrash size={19} />,
+    },
+    {
+      name: "Edit Field",
+      icon: <CiEdit size={19} />,
+    },
+  ];
+  return (
+    <div className="p-3 flex">
+      <SignOutButton className="bg-[#25231d]  rounded-sm  px-3">
+            Sign out
+      </SignOutButton>
+      <div className="flex gap-6 justify-end flex-grow">
+        {options.map((option) => (
+          <span className="flex items-center gap-1 text-sm hover:bg-[#25231d] p-1 px-3 rounded-md cursor-pointer">
+            {option.icon}
+            {option.name}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
 }
