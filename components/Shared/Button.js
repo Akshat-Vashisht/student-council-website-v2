90a4ef68bc689791ec4a90ee496751d68572f829
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-03-19 02:44:10 +0530

    add props

--- components/Shared/Button.js
+++ components/Shared/Button.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-function Button() {
+function Button(width) {
   return (
     <div>
-      <button className="justify-center items-center px-16 py-1.5 mt-8 max-w-full text-white whitespace-nowrap bg-stone-950 rounded-[36px] tracking-[2px] w-[359px] max-md:px-5">
+      <button
+        className={`justify-center items-center px-16 py-1.5 mt-8  text-white whitespace-nowrap bg-stone-950 rounded-[36px] tracking-[2px] w-${width} max-md:px-5`}
+      >
         LEARN MORE
       </button>
     </div>
