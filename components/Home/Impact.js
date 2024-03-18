4b971da1f3f740579ebbae71f8a1c17647c4fcd4
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-03-18 22:07:42 +0530

    fix:  responbsiveness

--- components/Home/Impact.js
+++ components/Home/Impact.js
@@ -2,27 +2,24 @@ import React from "react";
 
 function Impact() {
   return (
-    <div>
-      {" "}
-      <div className="px-12 py-14 bg-amber-100 max-md:px-5">
-        <div className="flex gap-5 max-md:flex-col max-md:gap-0">
-          <div className="flex flex-col w-[33%] max-md:ml-0 max-md:w-full">
-            <div className="flex flex-col grow items-center px-20 py-16 w-full font-bold text-justify whitespace-nowrap bg-sky-300 rounded-xl text-teal-950 max-md:px-5 max-md:mt-10 max-md:max-w-full">
-              <div className="text-9xl max-md:text-4xl">IM</div>
-              <div className="text-2xl">Innovative Minds</div>
-            </div>
+    <div className="px-4 md:px-12 py-14 bg-[#FFECC1]">
+      <div className="flex flex-col md:flex-row md:gap-5">
+        <div className="mb-5 md:mb-0 w-full md:w-1/3">
+          <div className="flex flex-col items-center px-8 md:px-20 py-8 md:py-16 w-full font-bold text-justify bg-[#79E0E6] rounded-xl text-teal-950">
+            <div className="text-4xl md:text-9xl">IM</div>
+            <div className="text-lg md:text-2xl">Innovative Minds</div>
           </div>
-          <div className="flex flex-col ml-5 w-[33%] max-md:ml-0 max-md:w-full">
-            <div className="flex flex-col grow items-center px-20 py-16 w-full font-bold text-justify whitespace-nowrap bg-lime-400 rounded-xl text-teal-950 max-md:px-5 max-md:mt-10 max-md:max-w-full">
-              <div className="text-9xl max-md:text-4xl">PA</div>
-              <div className="text-2xl">Promoting Achievement</div>
-            </div>
+        </div>
+        <div className="mb-5 md:mb-0 w-full md:w-1/3">
+          <div className="flex flex-col items-center px-8 md:px-20 py-8 md:py-16 w-full font-bold text-justify bg-[#A7C643] rounded-xl text-teal-950">
+            <div className="text-4xl md:text-9xl">PA</div>
+            <div className="text-lg md:text-2xl">Promoting Achievement</div>
           </div>
-          <div className="flex flex-col ml-5 w-[33%] max-md:ml-0 max-md:w-full">
-            <div className="flex flex-col grow items-center px-20 py-16 w-full font-bold text-justify whitespace-nowrap bg-orange-400 rounded-xl text-teal-950 max-md:px-5 max-md:mt-10 max-md:max-w-full">
-              <div className="text-9xl max-md:text-4xl">IM</div>
-              <div className="text-2xl">Cultivating Togetherness</div>
-            </div>
+        </div>
+        <div className="w-full md:w-1/3">
+          <div className="flex flex-col items-center px-8 md:px-20 py-8 md:py-16 w-full font-bold text-justify bg-[#F6A342] rounded-xl text-teal-950">
+            <div className="text-4xl md:text-9xl">IM</div>
+            <div className="text-lg md:text-2xl">Cultivating Togetherness</div>
           </div>
         </div>
       </div>
