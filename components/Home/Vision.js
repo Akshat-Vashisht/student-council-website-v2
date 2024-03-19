da15757892cbf5d40ebc2128eb4c5a7522e8b85c
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-03-20 03:32:21 +0530

    feat added vision mission objective carousel

--- components/Home/Vision.js
+++ components/Home/Vision.js
@@ -2,48 +2,86 @@ import React from "react";
 import { Carousel } from "antd";
 
 const contentStyle = {
-  height: "350px",
+  height: "750px",
   color: "#fff",
-  justyContent: "center",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
   textAlign: "center",
-  background: "#364d79",
+  background: "#4299e1",
   padding: "20px",
 };
 
+const scrollableContentStyle = {
+  ...contentStyle,
+  maxHeight: "650px",
+  overflowY: "auto",
+  scrollBehavior: "smooth",
+};
+
 const App = () => (
-  <Carousel autoplay className="p-20  rounded bg-[#FFECC1]">
-    <div>
-      <div style={contentStyle}>
-        <h3>Slide 1</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-          suscipit magna sit amet justo varius, id volutpat nulla dictum.
-          Vestibulum vestibulum tristique velit nec dignissim.
-        </p>
+  <div className="carousel-container overflow-y-auto max-h-96 lg:max-h-full smM:h-[550px] ">
+    <Carousel autoplay className="p-20 bg-[#FFECC1]">
+      <div>
+        <div
+          style={scrollableContentStyle}
+          className="flex flex-col text-xl smM:text-sm gap-16 smM:gap-6 smM:h-max"
+        >
+          <div className="text-6xl mdM:text-4xl smM:text-2xl font-bold">
+            <h3>VISION</h3>
+          </div>
+          <p>
+            To create a vibrant and cohesive student community at MIT-ADT
+            University, where every individual is empowered to excel
+            academically, professionally, and personally
+          </p>
+        </div>
       </div>
-    </div>
-    <div>
-      <div style={contentStyle}>
-        <h3>Slide 2</h3>
-        <p>
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-          ab illo inventore veritatis et quasi architecto beatae vitae dicta
-          sunt explicabo.
-        </p>
+      <div>
+        <div
+          style={scrollableContentStyle}
+          className="flex flex-col text-xl smM:text-sm gap-16 smM:gap-6 smM:h-max"
+        >
+          <div className="text-6xl mdM:text-4xl smM:text-2xl font-bold">
+            <h3>MISSION</h3>
+          </div>
+          <p>
+            The MIT-IMPACT Student Council is committed to fostering an
+            environment of inspiration, achievement, and togetherness by:
+            <br /> 1. Providing opportunities for intellectual growth and
+            creative exploration.
+            <br /> 2. Supporting student endeavors to achieve academic
+            excellence and personal development.
+            <br /> 3. Cultivating a sense of belonging and inclusivity through
+            diverse and engaging initiatives.
+            <br /> 4. Facilitating collaboration and synergy among students,
+            faculty, and staff to enhance the overall university experience.
+            <br /> 5. To create a supportive environment that empowers students
+            to excel academically, socially and personally.
+          </p>
+        </div>
       </div>
-    </div>
-    <div>
-      <div style={contentStyle}>
-        <h3>Slide 3</h3>
-        <p>
-          Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
-          fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem
-          sequi nesciunt.
-        </p>
+      <div>
+        <div
+          style={scrollableContentStyle}
+          className="flex flex-col text-xl smM:text-sm gap-16 smM:gap-6 smM:h-max"
+        >
+          <div className="text-6xl mdM:text-4xl smM:text-2xl font-bold">
+            <h3>OBJECTIVE</h3>
+          </div>
+          <p>
+            The MIT-IMPACT Student council aims to represent and unite the
+            MIT-ADT University community by advocating for student needs,
+            fostering inclusivity, and organizing events for personal and
+            leadership development. We facilitate dialogue between students,
+            faculty, and administration to address concerns and enact positive
+            change. Empowering individuals to engage in decision-making, we
+            strive to enhance the university experience for all
+          </p>
+        </div>
       </div>
-    </div>
-  </Carousel>
+    </Carousel>
+  </div>
 );
 
 export default App;
