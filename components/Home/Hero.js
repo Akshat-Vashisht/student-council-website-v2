7c7b6b00d156128a94ee5a715e6e698f2fafaa97
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-03-19 21:49:37 +0530

    fix:hero text

--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import Image from "next/image";
 import Star from "@components/Shared/Star";
 import Exclaim from "@components/Shared/Exclaim";
@@ -9,18 +9,17 @@ function Hero() {
 
   useEffect(() => {
     const handleResize = () => {
-      setIsPhone(window.innerWidth <= 654); 
+      setIsPhone(window.innerWidth <= 654);
     };
     handleResize();
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
   return (
- 
-      
+    <div>
       <div className="h-[90vh] mdM:h-fit relative bg-[#A7C643] flex mdM:flex-col gap-4 justify-between mdM:justify-center mdM:gap-8 items-center p-14 overflow-hidden">
         <div className="space-y-4 mdM:flex mdM:flex-col mdM:justify-center mdM:items-center">
           <h1 className="uppercase relative text-7xl w-[20rem] mdM:w-full mdM:text-center mdM:text-[10vw] font-bold ">
@@ -30,8 +29,8 @@ function Hero() {
               position={"-top-[7rem] right-10 mdM:left-3 mdM:-top-[5rem] "}
             />
             <span className="relative">
-            LET&apos;S <span className="block mdM:inline">BE THE </span>
-                CHANGE
+              LET&apos;S <span className="block mdM:inline">BE THE </span>
+              CHANGE
             </span>
           </h1>
           <h3 className="w-3/4 text-xl text-[#083133] font-black">
@@ -51,11 +50,26 @@ function Hero() {
           />
         </div>
         <Star
-            color={"#79E0E6"}
-            size={isPhone ? 100 : 220}
-            position={"-bottom-[2rem] -right-[2rem]"}
-          />
+          color={"#79E0E6"}
+          size={isPhone ? 100 : 220}
+          position={"-bottom-[2rem] -right-[2rem]"}
+        />
       </div>
+      <div className=" flex items-center border-[6px] mdM:w-full border-white rounded-[1.1rem]">
+        <Image
+          alt="hero-img"
+          className="rounded-xl block mdM:w-full mdM:h-auto"
+          src="/assets/Team/student_council_members.jpg"
+          width={700}
+          height={700}
+        />
+      </div>
+      <Star
+        color={"#79E0E6"}
+        size={220}
+        position={"-bottom-[2rem] -right-[6rem] smM:hidden"}
+      />
+    </div>
   );
 }
 
