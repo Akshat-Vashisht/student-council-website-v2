832c48dca0ba253cf11cb7e1988568f71023e426
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-03-19 19:50:24 +0530

    fix: responsiveness

--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -4,20 +4,20 @@ import Exclaim from "@components/Shared/Exclaim";
 import Button from "@components/Shared/Button";
 function Hero() {
   return (
-    <section class="text-gray-600 bg-[#A7C643] body-font overflow-y-hidden">
-      <div class="container mx-auto flex px-5 py-16 md:flex-row flex-col items-center">
+    <section class="text-gray-600 bg-[#A7C643] body-font overflow-hidden">
+      <div class="container mx-auto flex px-5 py-16 md:flex-row flex-col items-center relative">
         <div class="lg:flex-grow md:w-1/4 lg:pr-50 md:pr-40 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <div className="absolute ">
+          <h1 class="title-font sm:text-15xl text-8xl mb-4 font-medium text-gray-900 font-sans-pro z-20 relative">
+            {" "}
             <Star
               color={"#FCDE42"}
               size={180}
-              position={"-top-[5rem] -right-[20rem] "}
+              position={"-top-[6rem] -right-[0rem] "}
             />
-          </div>
-          <h1 class="title-font sm:text-15xl text-8xl mb-4 font-medium text-gray-900 font-sans-pro z-20">
-            {" "}
-            LET&apos;S <div>BE THE</div>
-            CHANGE
+            <span className="z-10 relative">
+              LET&apos;S <div>BE THE</div>
+              CHANGE
+            </span>
           </h1>
           <p class="text-gray-700 mb-4">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -28,7 +28,7 @@ function Hero() {
           </div>
         </div>
         {/* <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6"> */}
-        {/* Replace with the path to your image */}
+
         <Image
           className="rounded border-white border-3"
           height={800}
@@ -37,13 +37,11 @@ function Hero() {
           src="/assets/Team/student_council_members.jpg"
         ></Image>
         {/* </div> */}
-        <div className="absolute ">
-          <Star
-            color={"#79E0E6"}
-            size={180}
-            position={"-bottom-[22rem] -right-[90rem]"}
-          />
-        </div>
+        <Star
+          color={"#79E0E6"}
+          size={220}
+          position={"-bottom-[2rem] -right-[10rem]"}
+        />
       </div>
     </section>
   );
