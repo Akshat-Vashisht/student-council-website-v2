062dc07a11a5f6cc88630e25b2ad9fabf2c7fa50
Shankyy <sankalpt92@gmail.com>
2024-04-02 00:50:02 +0530

    feat(carousel): integrate event api

--- components/Events/Carousel.js
+++ components/Events/Carousel.js
@@ -1,6 +1,8 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Carousel } from "antd";
+import axios from "axios";
+
 const contentStyle = {
   height: "40rem",
   color: "#fff",
@@ -8,22 +10,39 @@ const contentStyle = {
   textAlign: "center",
   background: "#364d79",
 };
-const CarouselComponent = () => (
-  <div className="p-10 rounded bg-[#A7C643] ">
-    <Carousel effect="easing">
-      <div>
-        <h3 style={contentStyle}>1</h3>
-      </div>
-      <div>
-        <h3 style={contentStyle}>2</h3>
-      </div>
-      <div>
-        <h3 style={contentStyle}>3</h3>
-      </div>
-      <div>
-        <h3 style={contentStyle}>4</h3>
-      </div>
-    </Carousel>
-  </div>
-);
+
+const CarouselComponent = () => {
+  const [events, setEvents] = useState([])
+  async function fetchAllEvents(){
+    try {
+      const res = await axios.get("http://localhost:3000/api/events/getall")
+      setEvents(res.data.events)
+      console.log(res);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+   fetchAllEvents()
+  }, [])
+  
+  return (
+    <div className="p-10 rounded bg-[#A7C643] ">
+      <Carousel effect="easing">
+        <div>
+          <h3 style={contentStyle}>1</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>2</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>3</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>4</h3>
+        </div>
+      </Carousel>
+    </div>
+  );
+};
 export default CarouselComponent;
