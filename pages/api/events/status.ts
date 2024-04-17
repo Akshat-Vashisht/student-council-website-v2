109df808ffe4e29b7ea9bb20f50daab85b7d62df
davezaxh <zachariahdave@gmail.com>
2024-04-17 11:36:05 +0530

    fix: cors error in getall & status endpoints

--- pages/api/events/status.ts
+++ pages/api/events/status.ts
@@ -1,13 +1,18 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { MongoClient } from 'mongodb';
+import Cors from 'cors';
+
+const cors = Cors({
+  methods: 'GET',
+});
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+    await cors(req, res);
     const uri = process.env.DB_URI;
     const dbName = "studentcouncil"; //Database name
     const eventCollection = "events"; // Replace with your database name
     const currentDate = new Date().toISOString();;
     
-
     try {
     if(req.method === "GET"){  
       if(uri !== undefined){
@@ -20,6 +25,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         const upcomingEvents = await events.find({start: {$gt: currentDate}}).toArray();
 
         res.status(200).json({past: pastEvents, upcoming: upcomingEvents});
+        console.log("Successful Fetch");
     }
     else{
       res.status(500).json({ message: "Internal Server Error" });
